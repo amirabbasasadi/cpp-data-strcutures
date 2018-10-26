ae88c631deea3f2e9ee58144f41a0d54d85e0745
amirabbas asadi <amir137825@gmail.com>
2018-10-26 16:11:10 +0330

    binary tree traverse

--- BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree.h
@@ -12,10 +12,15 @@ public:
       : _data(data), _left(left), _right(right) {
     // Node constructor
   }
+  /// node data
   T data() { return _data; }
+  /// left child
   inline BinaryTreeNode<T> *left() { return _left; }
+  /// right child
   inline BinaryTreeNode<T> *right() { return _right; }
+  /// set left child
   inline void setLeft(BinaryTreeNode<T> *node) { _left = node; }
+  /// set right child
   inline void setRight(BinaryTreeNode<T> *node) { _right = node; }
 
 private:
@@ -38,6 +43,24 @@ public:
       inorderTraverse(root->left(), lambda);
       lambda(root);
       inorderTraverse(root->right(), lambda);
+    }
+  }
+  // Preorder Traverse
+  static void inorderTraverse(BinaryTreeNode<T> *root,
+                              std::function<void(BinaryTreeNode<T> *)> lambda) {
+    if (root) {
+      lambda(root);
+      inorderTraverse(root->left(), lambda);
+      inorderTraverse(root->right(), lambda);
+    }
+  }
+  // Inorder Traverse
+  static void postorderTraverse(BinaryTreeNode<T> *root,
+                              std::function<void(BinaryTreeNode<T> *)> lambda) {
+    if (root) {
+      inorderTraverse(root->left(), lambda);
+      inorderTraverse(root->right(), lambda);
+      lambda(root);
     }
   }
 };
